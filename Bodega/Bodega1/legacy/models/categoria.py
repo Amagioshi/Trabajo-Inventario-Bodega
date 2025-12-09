9c718c25ef12da1d18c271d154d307bf85669fa8
Amagioshi <rocczalez@gmail.com>
2025-12-09 20:24:15 -0300

    se agregaron validadores para que no se acepten nombres con solo numeros en las categorias y productos

--- Bodega/Bodega1/legacy/models/categoria.py
+++ Bodega/Bodega1/legacy/models/categoria.py
@@ -2,12 +2,26 @@ class Categoria: # definimos la clase Categoria
     def __init__(self, id: int, nombre: str): # contruimos la clase con los atributos y señala el tipo de dato que deberia contener
         if id < 0: # rechaza id negativos
             raise ValueError(f"El ID {id} no puede ser negativo.") # detiene la ejecucion y muestra un mensaje de error si la regla no se cumple
+        
         if not nombre or not nombre.strip(): # rechaza nombre vacios o con solo espacios
             raise ValueError("El nombre no puede estar vacio.")
-        if len(nombre.strip()) < 2: # nos asegura que tenga por lo menos 2 caracteres validos
-            raise ValueError("El nombre debe tener almenos 2 caracteres.")
+        
+        
+        nombre_limpio = nombre.strip()
+        if len(nombre_limpio) < 2 or len(nombre_limpio) > 40: # nos asegura que tenga por lo menos 2 caracteres validos
+            raise ValueError("El nombre debe tener entre 2 y 40 caracteres.")
+        
+        
+        if nombre_limpio.isdigit():
+            raise ValueError("El nombre no puede contener solo numeros")
+        
+
+        if not any (c.isalpha() for c in nombre_limpio):
+            raise ValueError("El nombre debe contener al menos 2 letras")
+        
+        
         self.__id = id # se guarda Guarda los valores de forma privada e impedimos que se modificquen desde afuera
-        self.__nombre = nombre.strip()
+        self.__nombre = nombre_limpio
         
     @property # permite leer el valor del atributo pero no modificarlo
     def id(self):
--- Bodega/Bodega1/legacy/models/producto.py
+++ Bodega/Bodega1/legacy/models/producto.py
@@ -2,24 +2,48 @@ class Producto:
     def __init__(self, id: int, nombre: str, precio: float, stock: int, categoria_id: int, activo: bool = True):
         if id < 0:
             raise ValueError(f"El ID {id} no puede ser negativo.")
+        
+        
         if not nombre or not nombre.strip():
             raise ValueError(f"Entrada invalida: {nombre}El nombre no puede estar vacio.")
-        if len(nombre.strip()) < 2 or len(nombre.strip()) > 100:
+        
+        nombre_limpio = nombre.strip()
+        
+        if len(nombre_limpio) < 2 or len(nombre_limpio) > 100:
             raise ValueError(f"Entrada invalida: {nombre}.El nombre debe tener entre 2 y 100 caracteres.")
+        
+        if nombre_limpio.isdigit():
+            raise ValueError("El nombre no puede ser solo numeros")
+        
+        if not any(c.isalpha() for c in nombre_limpio):
+            raise ValueError(" El nombre debe tener al menos 2 letras")
+        
         if precio < 0: # el precio debe ser positivo y mayor a 0  
             raise ValueError(f"Entrada invalida {precio}. El precio debe ser mayor a 0.")
+        
+        
         if stock < 0: # el stock no puede ser negativo 
             raise ValueError(f"Entrada invalida {stock}. El stock no puede ser negativo.")
+        
+        
         if categoria_id < 0: # el id de la categoria de un producto no puede ser negativo
             raise ValueError(f"entrada invalida {categoria_id}. El ID debe ser mayor a 0.")
+        
+        
         if not isinstance(activo, bool):
             raise ValueError(f"Entrada invalida {activo}. el estado debe ser verdadero o falso")
+        
+        
+        
         self.__id = id
-        self.__nombre = nombre.strip()
+        self.__nombre = nombre_limpio
         self.__precio = precio
         self.__stock = stock
         self.__categoria_id = categoria_id
         self.__activo = activo
+    
+    
+    
     @property
     def id(self):
         return self.__id
